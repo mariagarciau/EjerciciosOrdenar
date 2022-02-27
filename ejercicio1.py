aaba73ce1779083aa795870173b7be4711e4762b
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-02-27 22:04:56 +0100

    ej1

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+def ordenInsercion(lista):
+    for i in range(len(lista)-1):
+        if lista[i+1]<lista[i]:
+            reubicar(lista, i+1)
+        print(lista)
+def reubicar(lista,p):
+    v=lista[p]
+    j=p
+    while j>0 and v<lista[j-1]:
+        lista[j]=lista[j-1]
+        j-=1
+    lista[j]=v
